53af8342f5b0507ae9777aff30796b4d8f949f7c
aidandomondon <93887720+aidandomondon@users.noreply.github.com>
2023-12-18 12:43:23 -0500

    Started `request_adversarial.py` to handle the request of adversarial examples from the model

---
+++ controller/request_adversarial.py
@@ -0,0 +1,4 @@
+###############################################################################
+# Requests an adversarial example that is misclassified by the current model
+# given the current data that has been provided by the user.
+###############################################################################
