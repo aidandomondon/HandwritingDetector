20dbd2db53eb69a6a3d7bd5b5b99e1ad1600b316
aidandomondon <93887720+aidandomondon@users.noreply.github.com>
2023-12-20 03:05:13 -0500

    * Now able to draw on canvas and send drawing to database on release click * Creates DrawingGridModel to represent the drawing canvas as a 2d array

---
+++ view/drawing_grid_model.py
@@ -0,0 +1,32 @@
+class DrawingGridModel():
+    '''
+    Two-dimensional array to internally represent the canvas state.
+
+    *This array is not managed in the model part of the program because
+    this is specific to this implementation of the view / interface 
+    (that is, a GUI where users can draw, as opposed to, for example, 
+    one where users can directly upload files of arrays).
+    '''
+        
+
+    def __init__(self, size):
+        '''
+        @param `size`   side length of the (square) canvas to be represented by
+                        this model
+        '''
+        self.size = size
+        self.arr = [[0 for j in range(size)] for i in range(size)]
+        
+
+    def write(self, i, j, value):
+        '''
+        Writes `value` into the cell at the `i`th row and `j`th column of
+        this model's array if (i, j) is within the bounds of the array. If not,
+        does nothing.
+
+        @param `i` the row of the cell being written to
+        @param `j` the column of the cell being written to
+        @param `value` the value to write to the cell
+        '''
+        if j >= 0 and j < self.size and i >= 0 and i < self.size:
+            self.arr[i][j] = value
--- view/training_tab.py
+++ view/training_tab.py
@@ -1,46 +1,39 @@
 import tkinter as tk
 import tkinter.ttk as ttk
 from controller import add_training_image
+from view import drawing_grid_model
 
 FIXED_SIZE = 28
 
 # Fills the 4-neighborhood of the given point on the given canvas.
 # Fills the point's neighbors with a lighter color than used for point.
 # Updates the internal representation of the grid to reflect the added stroke.
-def _stroke(canvas, internal_model, x, y):
+def _stroke(canvas, internal_model :drawing_grid_model.DrawingGridModel, x, y):
     full_color = "#000000"
     light_color = "#808080"
     full_color_internal = 255   # pixel intensity to be given to the center
     light_color_internal = 128  # pixel intensity to be given to the neighbors
     r = 1
 
-    def check_range(x, y):
-        return x >= 0 and x < FIXED_SIZE and y >= 0 and y < FIXED_SIZE
-
     # Center
-    if check_range(x, y):
-        canvas.create_rectangle(x, y, x, y, fill=full_color)
-        internal_model[y][x] = full_color_internal
+    canvas.create_rectangle(x, y, x, y, fill=full_color)
+    internal_model.write(y, x, full_color_internal)
 
     # Left neighbor
-    if check_range(x - r, y):
-        canvas.create_rectangle(x - r, y, x - r, y, fill=light_color)
-        internal_model[y][x - r] = light_color_internal
+    canvas.create_rectangle(x - r, y, x - r, y, fill=light_color)
+    internal_model.write(y, x - r, light_color_internal)
 
     # Right neighbor
-    if check_range(x + r, y):
-        canvas.create_rectangle(x + r, y, x + r, y, fill=light_color)
-        internal_model[y][x + r] = light_color_internal
+    canvas.create_rectangle(x + r, y, x + r, y, fill=light_color)
+    internal_model.write(y, x + r, light_color_internal)
 
     # Bottom neighbor
-    if check_range(x, y - r):
-        canvas.create_rectangle(x, y - r, x, y - r, fill=light_color)
-        internal_model[y - r][x] = light_color_internal
+    canvas.create_rectangle(x, y - r, x, y - r, fill=light_color)
+    internal_model.write(y - r, x, light_color_internal)
 
     # Top neighbor
-    if check_range(x, y + r):
-        canvas.create_rectangle(x, y + r, x, y + r, fill=light_color)
-        internal_model[y + r][x] = light_color_internal
+    canvas.create_rectangle(x, y + r, x, y + r, fill=light_color)
+    internal_model.write(y + r, x, light_color_internal)
 
 
 def training_tab(tab_view :ttk.Notebook):
@@ -57,12 +50,14 @@ def training_tab(tab_view :ttk.Notebook):
     training_tab.pack(expand=True, fill='both')
     tab_view.add(training_tab, text='Train')
 
+    target = 7
+
     ###############
     # Text (Prompt)
     ###############
 
     # prompt users on what number to draw
-    training_tab_prompt = ttk.Label(training_tab, text=f"Draw a {7}.")
+    training_tab_prompt = ttk.Label(training_tab, text=f"Draw a {target}.")
     training_tab_prompt.pack(expand=True, fill='both')
 
     #############
@@ -70,11 +65,7 @@ def training_tab(tab_view :ttk.Notebook):
     #############
 
     # Initialize 2d array as internal representation of the canvas state.
-    # **    This array is not managed in the model part of the program because 
-    #       this is specific to this implementation of the view / interface 
-    #       (that is, a GUI where users can draw, as opposed to, for example, 
-    #       one where users can directly upload files of arrays)
-    model = [[0 for j in range(FIXED_SIZE)] for i in range(FIXED_SIZE)]
+    model = drawing_grid_model.DrawingGridModel(FIXED_SIZE)
 
     # Initialize actual canvas that users can draw on
     drawing_pad = tk.Canvas(training_tab, width=FIXED_SIZE, height=FIXED_SIZE)
@@ -84,5 +75,7 @@ def training_tab(tab_view :ttk.Notebook):
         _stroke(drawing_pad, model, event.x, event.y)
     drawing_pad.bind("<B1-Motion>", _f)
     drawing_pad.bind("<Button-1>", _f)
-    # drawing_pad.bind("<ButtonRelease-1>", add_training_image.__main__())
+    drawing_pad.bind("<ButtonRelease-1>", 
+                     lambda x: add_training_image.__main__(model.arr, target))
+
     return training_tab
