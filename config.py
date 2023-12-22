e332b3212704b2e88d78032478d2862618793d2c
aidandomondon <93887720+aidandomondon@users.noreply.github.com>
2023-12-21 23:28:38 -0500

    Remove `NEW_IMAGES_CACHE_DIR` configuration

--- config.py
+++ config.py
@@ -4,7 +4,6 @@ class Config():
     '''
     PROJECT_ROOT_DIR = None
     DB_PATH = None
-    NEW_IMAGES_CACHE_DIR = None
     MODEL_SAVE_PATH = None
     EPOCHS = None
     IMAGE_ENCODING_METHOD = None
