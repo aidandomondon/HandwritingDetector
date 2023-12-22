d2a0f599953d65b916e51000af4ab6595d315811
aidandomondon <93887720+aidandomondon@users.noreply.github.com>
2023-12-22 00:03:14 -0500

    Update to match new configuration file system

--- model/ml/train.py
+++ model/ml/train.py
@@ -1,35 +1,31 @@
 ###############################################################################
 # Trains classifier on given images.
 ###############################################################################
+from config import Config
 import os
 import torch.optim.adam
-import json
 import torch.utils.data.dataloader as dl
 import torch.nn as nn
-
-# Configurations
-with open('config.json', 'r') as jsonfile:
-    config = json.load(jsonfile)
 
 
 # 1. Load classifier if continuing training, else create new classifier.
 # Avoids retraining on old data, which could be superfluous and thus inefficient
 # or even lead to overfitting.
 def _loadModel(default_arch):
-    saves = os.listdir(config.MODEL_SAVE_PATH)
+    saves = os.listdir(Config.MODEL_SAVE_PATH)
     if saves == []:
         net = default_arch()
     else:
-        net = torch.load(config.MODEL_SAVE_PATH)
+        net = torch.load(Config.MODEL_SAVE_PATH)
     return net
 
 
 # 2. Train classifier
-def _train(net, loss_fn, dataloader :dl.DataLoader):
+def _train(net :nn.Module, loss_fn, dataloader :dl.DataLoader):
     optimizer = torch.optim.Adam(net.parameters())
-    for epoch in config.epochs:
-        for i, data in enumerate(dataloader):
-            images, labels = data
+    for epoch in Config.EPOCHS:
+        for _, data in enumerate(dataloader):
+            images, labels = data   # unzip
             optimizer.zero_grad()
             logits = net.forward(images)
             loss = loss_fn(logits, labels)
@@ -39,7 +35,7 @@ def _train(net, loss_fn, dataloader :dl.DataLoader):
 
 # 3. Save classifier state
 def _saveModel(net):
-    torch.save(net, config.MODEL_SAVE_PATH)
+    torch.save(net, Config.MODEL_SAVE_PATH)
 
 
 def __main___(dataloader :dl.DataLoader, default_arch :nn.Module, loss_fn):
